73fea9d8cad97ec6b0af8fc1a8e13eb8e69ab29d
Louis Martins-Roxo <SESA545164@se.com>
2019-08-23 11:43:49 +0200

    added dist

---
+++ dist/docs/js/build-config.js
@@ -0,0 +1,7 @@
+angular.module('docsApp').constant('BUILDCONFIG', {
+  "ngVersion": "1.7.7",
+  "version": "1.1.20",
+  "repository": "https://github.com/angular/material",
+  "commit": "a4f917fef75dde60a8f2e6eacb9bf78fd9b2a3b3",
+  "date": "2019-08-23 10:53:26 +0200"
+});
